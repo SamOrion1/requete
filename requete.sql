ff80e1adf3d9b4311452cee80e052f0933443d0a
Sam <sami_kherbache@hotmail.com>
2022-04-08 17:03:50 +0200

    second commit

--- requete.sql
+++ requete.sql
@@ -1,3 +1,5 @@
+--==========DEBUT SLIDE 42==========--
+
 --Liste nom hero -> ordre alphabétique
 SELECT Nom FROM Heros
 ORDER BY Nom
@@ -22,15 +24,56 @@ LIMIT 1
 --Calcule PV de tout personnage
 SELECT SUM(PV) FROM Heros
 
---Liste nom personnage + compétence (fonctionne pas, à faire)
-SELECT Nom, CompetenceID
+--Liste nom personnage + compétence 
+SELECT Heros.Nom, CompetenceHeros.Nom
 FROM Heros
-INNER JOIN Competence-Heros
-ON Competence-Heros.CompetenceID = Heros.ID
+JOIN CompetenceHeros
+ON CompetenceHeros.CompetenceID = Heros.ID
 
---Liste compétences : nom, types, effet (fonctionne pas, à faire)
-SELECT Effet, `Type`, Nom
+--Liste compétences : nom, types, effet 
+SELECT Competence.`Type`, Competence.Effet, CompetenceHeros.Nom
 FROM Competence
-INNER JOIN Competence-Heros
-ON Competence.Effet, `Type`=Competence-Heros.Nom
+JOIN CompetenceHeros
+ON CompetenceHeros.CompetenceID = Competence.ID
 
+--Liste compétence partagé + nom hero
+SELECT CompetenceHeros.Nom, Heros.Nom 
+FROM CompetenceHeros
+JOIN Heros
+ON CompetenceHeros.HerosID = Heros.ID
+WHERE CompetenceHeros.Nom IN
+(
+   SELECT CompetenceHeros.Nom FROM CompetenceHeros
+   GROUP BY CompetenceHeros.Nom
+   HAVING COUNT(CompetenceHeros.Nom)>1
+)
+
+--Liste compétence partagé
+SELECT DISTINCT CompetenceHeros.Nom 
+FROM CompetenceHeros
+JOIN Heros
+ON CompetenceHeros.HerosID = Heros.ID
+WHERE CompetenceHeros.Nom IN
+(
+   SELECT CompetenceHeros.Nom FROM CompetenceHeros
+   GROUP BY CompetenceHeros.Nom
+   HAVING COUNT(CompetenceHeros.Nom)>1
+)
+
+--Liste personnage qui on une Arme
+SELECT Nom
+FROM Heros
+WHERE ArmeEnMain = 1 
+
+--Liste personnage avec arme + competence
+SELECT Heros.Nom, CompetenceHeros.Nom
+FROM Heros
+JOIN CompetenceHeros
+ON CompetenceHeros.HerosID = Heros.ArmeEnMain
+
+--==========FIN SLIDE 42==========--
+
+
+
+
+--==========DEBUT SLIDE 43==========--
